745211e7ff8c4c1c74b8bc5ecd86207505db6901
Kelvin3c <englandisme123@gmail.com>
2023-09-21 04:01:53 +0500

    .

--- conditional/conditional.js
+++ conditional/conditional.js
@@ -1,7 +1,14 @@
+// Check if a number is odd or even in JavaScript
 function isEvenOrOdd(number){
 
-    if(number%2 === 0){
-        return 
-    }
+    document.getElementById("theNumber").innerHTML = "The Number Is: " + arguments[0]
 
+    if(number%2 === 0){
+        return document.getElementById("oddOrEven").innerHTML = "The Number Is Even"
+    }else{
+        return document.getElementById("oddOrEven").innerHTML = "The Number Is Odd"
+    }
 }
+// console.log(isEvenOrOdd(5));
+isEvenOrOdd(5);
+
